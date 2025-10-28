6b63c098382b1d7b29cf3811e56e1f13b54b219f
难为水 <NoWaterisEnough@outlook.com>
2025-10-28 17:26:28 +0800

    style: 调整代码格式以符合 clang-format 规范
    - 调整 main.c 和 main.h 的代码格式
    - 统一 board_config.h 的宏定义格式
    - 调整 hal_timebase.c 的大括号风格
    - 所有改动均由 clang-format 自动完成

--- app/main.h
+++ app/main.h
@@ -2,14 +2,15 @@
 #define __MAIN_H
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-/* 统一只包含板级头，由板选择正确系列的 HAL/CMSIS 头 */
+    /* 统一只包含板级头，由板选择正确系列的 HAL/CMSIS 头 */
 #include "board.h"
 
-/* 导出函数 */
-void Error_Handler(void);
+    /* 导出函数 */
+    void Error_Handler(void);
 
 #ifdef __cplusplus
 }
